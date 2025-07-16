cd6d879026dad91c89fa292251d8e4f2707b6da0
Your Name <sanyok185@gmail.com>
2025-07-16 12:28:22 +0300

    Updates

--- js/dev/index.js
+++ js/dev/index.js
@@ -35,60 +35,6 @@
     fetch(link.href, fetchOpts);
   }
 })();
-class MousePRLX {
-  constructor(props, data = null) {
-    let defaultConfig = {
-      init: true
-    };
-    this.config = Object.assign(defaultConfig, props);
-    if (this.config.init) {
-      const paralaxMouse = document.querySelectorAll("[data-fls-mouse]");
-      if (paralaxMouse.length) {
-        this.paralaxMouseInit(paralaxMouse);
-      }
-    }
-  }
-  paralaxMouseInit(paralaxMouse) {
-    paralaxMouse.forEach((el) => {
-      const paralaxMouseWrapper = el.closest("[data-fls-mouse-wrapper]");
-      const paramСoefficientX = el.dataset.flsMouseCx ? +el.dataset.flsMouseCx : 100;
-      const paramСoefficientY = el.dataset.prlxCy ? +el.dataset.prlxCy : 100;
-      const directionX = el.hasAttribute("data-fls-mouse-dxr") ? -1 : 1;
-      const directionY = el.hasAttribute("data-fls-mouse-dyr") ? -1 : 1;
-      const paramAnimation = el.dataset.prlxA ? +el.dataset.prlxA : 50;
-      let positionX = 0, positionY = 0;
-      let coordXprocent = 0, coordYprocent = 0;
-      setMouseParallaxStyle();
-      if (paralaxMouseWrapper) {
-        mouseMoveParalax(paralaxMouseWrapper);
-      } else {
-        mouseMoveParalax();
-      }
-      function setMouseParallaxStyle() {
-        const distX = coordXprocent - positionX;
-        const distY = coordYprocent - positionY;
-        positionX = positionX + distX * paramAnimation / 1e3;
-        positionY = positionY + distY * paramAnimation / 1e3;
-        el.style.cssText = `transform: translate3D(${directionX * positionX / (paramСoefficientX / 10)}%,${directionY * positionY / (paramСoefficientY / 10)}%,0) rotate(0.02deg);`;
-        requestAnimationFrame(setMouseParallaxStyle);
-      }
-      function mouseMoveParalax(wrapper = window) {
-        wrapper.addEventListener("mousemove", function(e) {
-          const offsetTop = el.getBoundingClientRect().top + window.scrollY;
-          if (offsetTop >= window.scrollY || offsetTop + el.offsetHeight >= window.scrollY) {
-            const parallaxWidth = window.innerWidth;
-            const parallaxHeight = window.innerHeight;
-            const coordX = e.clientX - parallaxWidth / 2;
-            const coordY = e.clientY - parallaxHeight / 2;
-            coordXprocent = coordX / parallaxWidth * 100;
-            coordYprocent = coordY / parallaxHeight * 100;
-          }
-        });
-      }
-    });
-  }
-}
-document.querySelector("[data-fls-mouse]") ? window.addEventListener("load", new MousePRLX({})) : null;
 const marquee = () => {
   const $marqueeArray = document.querySelectorAll("[data-fls-marquee]");
   const ATTR_NAMES = {
